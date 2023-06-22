3ac220be3667c9c42b7c1b37eba34be8ebe39827
Jorge Armando Laban Hijar <83684476+jarlh19@users.noreply.github.com>
2023-06-22 17:27:53 -0500

    update Travel-list

--- src/app/travel-list/travel-list.component.ts
+++ src/app/travel-list/travel-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-travel-list',
   templateUrl: './travel-list.component.html',
-  styleUrls: ['./travel-list.component.css']
+  styleUrls: ['./travel-list.component.css'],
 })
-export class TravelListComponent {
+
+
+export class TravelListComponent implements OnInit{
+  title = 'angularproject';
+  addElements(){
+    Swal.fire({
+      icon: 'success',
+      title: 'Element added',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
+  constructor() {
+  }
+  ngOnInit():void {
+
+  }
 
 }
