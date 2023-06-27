37d4e721e0172463373905ed0db6522021a849b6
Jorge Armando Laban Hijar <83684476+jarlh19@users.noreply.github.com>
2023-06-26 22:56:14 -0500

    update list view

--- src/app/travel-list/travel-list.component.ts
+++ src/app/travel-list/travel-list.component.ts
@@ -1,28 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-travel-list',
   templateUrl: './travel-list.component.html',
-  styleUrls: ['./travel-list.component.css'],
+  styleUrls: ['./travel-list.component.css']
+
 })
 
 
 export class TravelListComponent implements OnInit{
   title = 'angularproject';
-  addElements(){
-    Swal.fire({
-      icon: 'success',
-      title: 'Element added',
-      showConfirmButton: false,
-      timer: 1500
-    })
-  }
 
+  districts: District[] = [
+    {value: 'San Miguel-0', viewValue: 'San Miguel'},
+    {value: 'Magdalena-1', viewValue: 'Magdalena'},
+    {value: 'Miraflores-2', viewValue: 'Miraflores'},
+    {value: 'San Isidro-3', viewValue: 'San Isidro'},
+    {value: 'Barranco-4', viewValue: 'Barranco'},
+    {value: 'Surco-5', viewValue: 'Surco'},
+    {value: 'San Borja-6', viewValue: 'San Borja'},
+  ];
+  preferences: Preference[] = [
+    {value: 'playa-0', viewValue: 'Playa'},
+    {value: 'bosque-1', viewValue: 'Bosque'},
+    {value: 'treking-2', viewValue: 'Treking'},
+    {value: 'aventura-3', viewValue: 'Aventura'},
+    {value: 'cultura-4', viewValue: 'Cultura'},
+    {value: 'gastronomia-5', viewValue: 'Gastronomia'},
+    {value: 'historia-6', viewValue: 'Historia'},
+    {value: 'deportes-7', viewValue: 'Deportes'},
+    {value: 'relax-8', viewValue: 'Relax'},
+    {value: 'vida nocturna-9', viewValue: 'Vida nocturna'},
+    {value: 'compras-10', viewValue: 'Compras'},
+  ];
   constructor() {
+
   }
   ngOnInit():void {
 
   }
 
+
+}
+interface District {
+  value: string;
+  viewValue: string;
+}
+interface Preference {
+  value: string;
+  viewValue: string;
 }
